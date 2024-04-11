71aca78241dcc48dafac5e3e4af7691a77fbf25e
Jr <jr.jimeno@me.com>
2024-04-10 23:24:41 -0400

    typo in comment

--- etl.py
+++ etl.py
@@ -19,15 +19,16 @@ df1 = pd.json_normalize(response_txt)
 #dropping columns we dont need
 df2 = df1.drop(columns=['tier', 'id', 'images.box.og', 'images.box.sm', 'images.banner.og', 'images.banner.sm'])
 
-#Removing time from date
+#removing time from date
 df2['releaseDate'] = pd.to_datetime(df2['firstReleaseDate']).dt.date
 
 df3 = df2.drop('firstReleaseDate', axis=1)
 
-#Rearranging columns
+#rearranging columns
 df3 = df3[['name', 'releaseDate', 'topCriticScore']]
 df3.sort_values(by=['topCriticScore'], inplace=True, ascending=False)
 
+#exporting to JSON and CSV
 df3.to_json('game-list.json', orient='records', indent=2)
 df3.to_csv('game-list.csv', index=False)
 
